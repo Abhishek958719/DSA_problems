99c8e148bb37f14a75b3f0a2d56411cbe08135db
Abhishek <abhishek98396@gmail.com>
2023-09-30 14:37:59 +0530

    SwapTwoNumberByBitManupulation

--- src/BitManupulation/SwapTwoNumber.java
+++ src/BitManupulation/SwapTwoNumber.java
@@ -1,5 +1,18 @@
 package BitManupulation;
 
-public class SwapTwoNumber {
+import java.util.Scanner;
 
+public class SwapTwoNumber {
+    public static void main(String []argv){
+        Scanner in = new Scanner(System.in);
+
+        int a = in.nextInt();
+        int b = in.nextInt();
+
+        a = a^b;
+        b = a^b;
+        a = a^b;
+
+        System.out.println("a = " + a + " b = " + b);
+    }
 }
